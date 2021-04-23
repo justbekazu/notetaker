40fb0c4db94c59999766db14684eef48d9bc69ea
djkaz <justbekazu@gmail.com>
2021-04-23 14:49:52 -0500

    setting up files

---
+++ server.js
@@ -0,0 +1,22 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express(); 
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+  // parse incoming string or array data
+  app.use(express.urlencoded({ extended: true }));
+  // parse incoming JSON data
+  app.use(express.json());
+
+    app.use('/api', apiRoutes);
+    app.use('/', htmlRoutes);
+
+
+ 
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+    });
