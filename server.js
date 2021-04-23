833924ddd559c76c112dd4a61d021b800b555e73
djkaz <justbekazu@gmail.com>
2021-04-23 16:53:54 -0500

    adding routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const express = require('express');
 const app = express();
 const path = require("path");
+const { resolveSoa } = require("dns");
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,11 +12,20 @@ const PORT = process.env.PORT || 3001;
   // parse incoming JSON data
   app.use(express.json());
 
+  app.use(express.static("./Develop/public"));
+
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname,"./Develop/public/notes.html"))
 })
 
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname,"./Develop/public/index.html"))
+})
 
+app.get("/api/notes", function(req, res) {
+   var noteFile = fs.readFileSync(path.join(__dirname,"./Develop/db/db.json"))
+    console.log(noteFile)
+})
 
     //const { notes } = require('.Develop/db.json');
 //const { Router } = require("express");
