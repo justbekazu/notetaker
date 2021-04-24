d0ad2902fe605b758e7aa56c9bca5479e9f8a4f2
djkaz <justbekazu@gmail.com>
2021-04-24 16:22:51 -0500

    adding uniqid

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const fs = require("fs");
 const express = require('express');
 const app = express();
 const path = require("path");
-const { resolveSoa } = require("dns");
+//const { resolveSoa } = require("dns");
+const uniqid = require("uniqid");
+
+console.log(uniqid);
 
 const PORT = process.env.PORT || 3001;
 
@@ -18,24 +21,26 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname,"./Develop/public/notes.html"))
 })
 
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname,"./Develop/public/index.html"))
-})
+
 
 app.get("/api/notes", function(req, res) {
-   var noteFile = fs.readFileSync(path.join(__dirname,"./Develop/db/db.json"))
-    console.log(noteFile)
+   var noteFile = fs.readFileSync(path.join(__dirname,"./Develop/db/db.json"), "utf8")
+    res.json(JSON.parse(noteFile))
+
+    console.log(noteFile);
 })
 
     //const { notes } = require('.Develop/db.json');
 //const { Router } = require("express");
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname,"./Develop/public/index.html"))
+})
 
 
-
-    app.delete('/api/notes/:id', function(req, res, next) {
-        req.params.id
-        res.send({type:'DELETE'});
-    });
+    // app.delete('/api/notes/:id', function(req, res, next) {
+    //     req.params.id
+    //     res.send({type:'DELETE'});
+    // });
 
  
 app.listen(3001, () => {
